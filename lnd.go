e1de461af0268aa4c8e02d05a4110413ef4c153c
Anthony Ronning <anthonyronning@gmail.com>
2019-07-09 00:27:52 -0500

    rpcserver: add cors support

--- lnd.go
+++ lnd.go
@@ -863,7 +863,7 @@ func waitForWalletPassword(restEndpoints []net.Addr,
 		return nil, err
 	}
 
-	srv := &http.Server{Handler: mux}
+	srv := &http.Server{Handler: allowCORS(mux)}
 
 	for _, restEndpoint := range restEndpoints {
 		lis, err := lncfg.TLSListenOnAddress(restEndpoint, tlsConf)
